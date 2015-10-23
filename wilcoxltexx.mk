ae02a910882cb94d7e8ef91633711ef9501ed0df
Arne Coucheron <arco68@gmail.com>
2015-10-23 02:04:56 +0200

    Shim library for missing symbols in Samsung blobs
     * Provide missing symbol needed for Samsung RIL
     * rild binary and libril.so library needs to be replaced by ones
       from a lollipop ROM. The ones from kitkat will simply crash.
    
    Change-Id: I50d4c89727950e1a8c238383e77e3855333e43f9
    
    Conflicts:
    	wilcoxltexx.mk

--- wilcoxltexx.mk
+++ wilcoxltexx.mk
@@ -127,6 +127,10 @@ PRODUCT_PACKAGES += \
     fsck.f2fs \
     mkfs.f2fs
 
+# Samsung symbols
+PRODUCT_PACKAGES += \
+    libsamsung_symbols
+
 # GPS/location security configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
