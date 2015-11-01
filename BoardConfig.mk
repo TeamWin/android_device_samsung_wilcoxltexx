4b84c15206c959bdeb1a523979b5cd3ef2ec402e
Kostas Tziridis <kostoulhs@users.noreply.github.com>
2015-11-01 14:27:39 +0200

    Fix target kernel source to make it co-exist

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_KERNEL_CMDLINE         := androidboot.hardware=qcom user_debug=23 androidb
 BOARD_KERNEL_BASE            := 0x80200000
 BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE        := 2048
-TARGET_KERNEL_SOURCE         := kernel/samsung/msm8930
+TARGET_KERNEL_SOURCE         := kernel/samsung/wilcoxltexx
 TARGET_KERNEL_CONFIG         := cyanogen_cane_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8930_cane_wilcox_eur_lte_defconfig
 
